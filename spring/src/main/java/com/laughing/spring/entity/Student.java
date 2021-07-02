16f4e9702104ca5fa352d2b14d7c291641ec5529
ximingze <425984393@qq.com>
2021-07-02 21:55:28 +0800

    set方法注入

---
+++ spring/src/main/java/com/laughing/spring/entity/Student.java
@@ -0,0 +1,27 @@
+package com.laughing.spring.entity;
+
+/**
+ * @author : laughing
+ * @create : 2021-07-02 21:48
+ * @description :
+ */
+public class Student {
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- spring/src/test/java/com/laughing/spring/test/SpringTest.java
+++ spring/src/test/java/com/laughing/spring/test/SpringTest.java
@@ -1,5 +1,6 @@
 package com.laughing.spring.test;
 
+import com.laughing.spring.entity.Student;
 import com.laughing.spring.service.SomeService;
 import com.laughing.spring.service.impl.SomeServiceImpl;
 import org.junit.Test;
@@ -12,6 +13,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @description :
  */
 public class SpringTest {
+    // 创建spring容器对象，创建容器对象时，会创建配置文件中的所有对象，默认调用无参构造器
+    ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+
     @Test
     public void test() {
         SomeService someService = new SomeServiceImpl();
@@ -20,10 +24,14 @@ public class SpringTest {
 
     @Test
     public void testIoC() {
-        // 创建spring容器对象
-        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         // 从容器中获取对象
         SomeService some = context.getBean("some", SomeService.class);
         some.doSome();
+    }
+
+    @Test
+    public void testDI() {
+        Student student = context.getBean("student", Student.class);
+        System.out.println(student);
     }
 }
