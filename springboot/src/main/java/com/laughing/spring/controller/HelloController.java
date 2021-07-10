80ba27490ae3b3cea7aee9bd5041b111b7b07f9e
ximingze <425984393@qq.com>
2021-07-10 20:06:46 +0800

    获取yaml文件中的value

--- springboot/src/main/java/com/laughing/spring/controller/HelloController.java
+++ springboot/src/main/java/com/laughing/spring/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.laughing.spring.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/test")
 public class HelloController {
+
+    @Value("${database}")
+    private String name;
+
+    @Value("${person.name}")
+    private String pName;
+
+    @Value("${language[0]}")
+    private String language;
+
+    @GetMapping("/name")
+    public String getName() {
+        return name + " " + pName + " " + language;
+    }
 
     @GetMapping("/hello")
     public String hello() {
