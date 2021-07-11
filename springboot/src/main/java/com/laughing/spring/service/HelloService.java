f8a67a96e2e7896e2f5f4cdfcc5dc9a78b3d93f0
ximingze <425984393@qq.com>
2021-07-11 19:52:08 +0800

    spring test

---
+++ springboot/src/main/java/com/laughing/spring/service/HelloService.java
@@ -0,0 +1,15 @@
+package com.laughing.spring.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author : laughing
+ * @create : 2021-07-11 19:41
+ * @description :
+ */
+@Service
+public class HelloService {
+    public void hello() {
+        System.out.println("hello...");
+    }
+}
--- springboot/src/test/java/com/laughing/spring/SpringbootApplicationTests.java
+++ springboot/src/test/java/com/laughing/spring/SpringbootApplicationTests.java
@@ -1,13 +1,18 @@
 package com.laughing.spring;
 
+import com.laughing.spring.service.HelloService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootApplicationTests {
+	@Autowired
+	private HelloService helloService;
 
 	@Test
 	void contextLoads() {
+		helloService.hello();
 	}
 
 }
